d317e9372d1ef04594e5b67ea37f7b3529654907
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-18 16:26:18 +0400

    looped through an array and returned a list of authors

--- app/routes/sff-authors.js
+++ app/routes/sff-authors.js
@@ -1,3 +1,7 @@
 import Route from '@ember/routing/route';
 
-export default class SffAuthorsRoute extends Route {}
+export default class SffAuthorsRoute extends Route {
+    model() {
+        return ['R.F Kuang', 'Nnedi Okoroafor', 'N.K Jemisin', 'S.A Chakraborty', 'Octavia Butler', 'Rebecca Roanhorse', 'Silvia Moreno-Garcia'];
+    }
+}
