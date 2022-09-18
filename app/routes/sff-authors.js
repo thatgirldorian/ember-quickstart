8e05ea126cabc47786c1e88de25ab273f2a54540
Debbie Otuagomah <thatgirldorian@gmail.com>
2022-09-19 02:04:33 +0400

    Wired up Tailwind CSS and added some styling

--- app/routes/sff-authors.js
+++ app/routes/sff-authors.js
@@ -1,15 +1,15 @@
 import Route from '@ember/routing/route';
 
 export default class SffAuthorsRoute extends Route {
-    model() {
-        return [
-        'R.F Kuang',
-        'Nnedi Okoroafor',
-        'N.K Jemisin',
-        'S.A Chakraborty',
-        'Octavia Butler',
-        'Rebecca Roanhorse',
-        'Silvia Moreno-Garcia',
-        ];
-    }
+  model() {
+    return [
+      'R.F Kuang',
+      'Nnedi Okoroafor',
+      'N.K Jemisin',
+      'S.A Chakraborty',
+      'Octavia Butler',
+      'Rebecca Roanhorse',
+      'Silvia Moreno-Garcia',
+    ];
+  }
 }
